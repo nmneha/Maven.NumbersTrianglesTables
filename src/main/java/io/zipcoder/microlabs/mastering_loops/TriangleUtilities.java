211bda704ba8f9e2fdb40a6f27b7908e28dd9243
Nusera <nusera@zipcoders-MacBook-Pro.local>
2022-02-28 21:20:15 -0500

    passed get row test

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,28 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+        for (int i = 0; i < numberOfRows; i++) {
+            triangle = triangle + "*\n";
+        }
+        return triangle;
     }
 
-    public static String getRow(int numberOfStars) {
-        return null;
+    public static String getRow(int width) {
+        String stars = "";
+        for (int i = 0; i < width; i++) {
+            stars = stars + "*";
+        }
+        return stars;
     }
 
     public static String getSmallTriangle() {
+
         return null;
     }
 
     public static String getLargeTriangle() {
+
         return null;
     }
 }
