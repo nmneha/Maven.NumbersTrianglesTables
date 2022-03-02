8ef4f27ad629889cac8fa101b1afd2cd35758c50
Nusera <nusera@zipcoders-MBP.fios-router.home>
2022-03-01 20:35:07 -0500

    passed tables tests

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,21 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return TableUtilities.getMultiplicationTable(4);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return TableUtilities.getMultiplicationTable(9);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String table = "";
+        for (int i = 1; i <= tableSize; i++) {
+            for (int n = 1; n <= tableSize; n++) {
+               table += String.format("%3d |", (i * n));
+            }
+            table = table + "\n";
+        }
+        return table;
     }
 }
